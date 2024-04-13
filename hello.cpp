255980f5e69def65558b14be05894b68aa98de69
WenZhiii <131173566+WenZhiii@users.noreply.github.com>
2024-04-13 22:16:23 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,20 @@
-#include<stdio.h>
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"hello world"<<endl;
-    cout<<"hello world"<<endl;
-}
+name: C/C++ Build and Run
+
+on: [push, pull_request]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Check out repository
+      uses: actions/checkout@v2
+
+    - name: Install build essentials
+      run: sudo apt-get install build-essential -y
+
+    - name: Build
+      run: g++ -o hello hello.cpp
+
+    - name: Run
+      run: ./hello
